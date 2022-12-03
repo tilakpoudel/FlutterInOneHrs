d8a805dbc7e5b493731a1ce78670fa031c78c67d
tilak <tilak.poudel.314@gmail.com>
2022-12-03 22:00:14 +0545

    feat: add profile pge

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutterinonehrs/explore_flutter_page.dart';
+import './explore_flutter_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutterinonehrs/home_page.dart';
+import './home_page.dart';
+import './profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,6 +28,10 @@ class RootPage extends StatefulWidget {
 
 class _RootPageState extends State<RootPage> {
   int currentPage = 0;
+  List<Widget> pages = const [
+    HomePage(),
+    ProfilePage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +39,7 @@ class _RootPageState extends State<RootPage> {
       appBar: AppBar(
         title: const Text('Flutter'),
       ),
-      body: const HomePage(),
+      body: pages[currentPage],
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           debugPrint('Floating action button clicked...');
